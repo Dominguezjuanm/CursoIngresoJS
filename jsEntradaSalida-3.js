104d543ebb457d9e05cd9095f76597571a5b6867
Dominguezjuanm <47418604+Dominguezjuanm@users.noreply.github.com>
2019-02-07 20:06:16 -0300

    Segunda practica de ejercicios
    ejercicios 1 a 5

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	
+	var nombre;
+	
+	//nombre = elNombre .value;
+	nombre = document.getElementById('elNombre').value;
+	
+	alert(nombre);
+
+
+}
+
+

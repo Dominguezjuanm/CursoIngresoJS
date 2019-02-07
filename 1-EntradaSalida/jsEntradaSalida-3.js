5707209cea76e5c4e1662878386389efa956e6a7
Dominguezjuanm <47418604+Dominguezjuanm@users.noreply.github.com>
2019-02-07 12:24:27 -0300

    Primeros ejercicios
    1 a 5

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -3,6 +3,14 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	
+	var nombre;
+	
+	nombre = elNombre .value;
+	nombre = document.getElementById('elNombre');
+	
+	alert(nombre);
+
+
 }
 
 

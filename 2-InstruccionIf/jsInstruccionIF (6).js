3e0dbebb1a4bcb29f81db65afd8e38a3967be5c5
alumno <alumno>
2019-02-14 12:01:53 -0300

    ejercicio 6 resuelto

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+	var laedad;
+
+	laedad= document.getElementById('edad').value;
+
+	if(laedad < 13){
+		alert("Sos un niño")
+	}else{
+		if(laedad < 17){
+			alert("Sos adolescente")
+		}else{
+			alert("Sos mayor de edad")
+		}
+	}
 
 
 

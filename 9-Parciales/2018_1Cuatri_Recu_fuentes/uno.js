3ca1c7e262923bd3521c9875587d0d076674d41d
alumno <alumno>
2019-02-15 09:37:04 -0300

    Ejercicio tp uno.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,27 @@
 
 function mostrar()
 {
+	var primermascota;
+	var segundamascota;
+	var pesouno;
+	var pesodos;
+	var sumadepeso
+	primermascota = prompt("Ingresa el nombre de la primer mascota");
+	segundamascota = prompt("ingresa el nombre de la segunda mascota");
 
+	pesouno = prompt("Ingrese el peso de la primer mascota");
+	pesodos = prompt("Ingrese el peso de la segunda mascota");
+
+	pesouno = parseint(pesouno);
+	pesodos = parseint(pesodos);
+
+	sumadepeso = (pesouno + pesodos);
+
+
+
+
+
+
+	alert("tenes dos mascotas" + primermascota + " y " + segundamascota +" , que pesan "+ pesouno+" y " +  pesodos + "kilos, la suma de los kilos es " +sumadepeso);
+	
 }

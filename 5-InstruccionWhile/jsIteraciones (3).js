6c4b30a03c9b864ca4fb933adefe898a0502f227
alumno <alumno>
2019-02-22 12:15:39 -0300

    ejercicios 1,2 y 3 while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave = prompt("ingrese el número clave.");
 
+
+	
+
+	while(clave != "utn750"){
+
+		clave = prompt("Error, ingrese el numero clave.");
+		
+		
+	}
+	alert("bienvenido/a");
 
 }//FIN DE LA FUNCIÓN

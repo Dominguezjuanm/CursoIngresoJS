f7f95664d4c78654436c2199403baa1aef06e346
Dominguezjuanm <47418604+Dominguezjuanm@users.noreply.github.com>
2019-07-04 11:25:09 -0300

    Ejercicios 1 a 10 InstruccionesIf

---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+//tomo la edad  
+	var laedad;
+
+	laedad= document.getElementById('edad').value;
+	
+	
+	if(laedad >= 18){ 
+		alert("Usted es mayor de edad")
+	}
+
+
+}//FIN DE LA FUNCIÓN

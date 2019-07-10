f3a33dfa5b1062c6e1659c74a105bc29a283d3bf
Dominguezjuanm <47418604+Dominguezjuanm@users.noreply.github.com>
2019-07-10 10:03:59 -0300

    TP FerreteConstruccion resuelto

--- jsFerreteConstruccion.js
+++ jsFerreteConstruccion.js
@@ -18,13 +18,28 @@ function Rectangulo ()
 }
 function Circulo () 
 {
-	
+	var elRadio;
+	elRadio = document.getElementById('Radio').value;
+	resultado = (2*Math.PI*elRadio)*3;
+	MostrarMensaje("La cantidad de alambre a comprar es; "+resultado);
 }
 function Materiales () 
 {
-	
+	var largo;
+	var	ancho;
+	var resultado;
+	var cemento;
+	var cal;
+	largo = document.getElementById('Largo').value;
+	ancho = document.getElementById('Ancho').value;
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+	cemento = ((largo*2)+(ancho*2))*2;
+	cal = ((largo*2)+(ancho*2))*3;
+	MostrarMensaje("Se necesitan "+cemento+" bolsas de cemento y "+cal+" bolsas de cal");	
 }
 function MostrarMensaje(resultado)
 {
 	alert(resultado);
 }
+//Para calcular con PI se usa Math.PI.
